7b075da88b80ec3cff6a7e0b71d6ebdb14322268
VictorRocha <146087933+victorramosrocha@users.noreply.github.com>
2025-12-28 07:19:11 -0300

    create index.php

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>DOM Teste</title>
+</head>
+<body>
+    <h1>Página de Testes</h1>
+    <p>
+        Este é apenas um teste para saber se da para utilizar o github como servidor para um site em PHP.
+    </p>
+</body>
+</html>
